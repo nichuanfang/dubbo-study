cedf82a9b4c45873cf042f1ea299769d03c47a4e
nichuanfang <1290274972@qq.com>
2019-04-14 22:32:17 +0800

    v1.1

---
+++ sell/src/main/java/com/chuanfangn/sell/service/OrderService.java
@@ -0,0 +1,11 @@
+package com.chuanfangn.sell.service;
+
+/**
+ * @description: 订单创建, 更改, 删除, 查询业务
+ * @author: 1290274972@qq.com
+ * @program: sell
+ * @create: 2019-04-14 22:30
+ * @version: 1.0
+ **/
+public class OrderService {
+}
